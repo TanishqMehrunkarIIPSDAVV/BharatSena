7bf81deb336aeb6616a3902cb4be48d6b7ba7293
TanishqMehrunkarIIPSDAVV <mehrunkart@gmail.com>
2025-08-12 18:02:29 +0530

    bg update

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,18 +1,14 @@
-import bg from "../assets/videos/flag.mp4";
+import flagImage from '../assets/images/flag.png';
 
 const Background = ()=>
 {
   return (
-    <div className="fixed top-0 left-0 w-full h-full">
-      <video
-        autoPlay
-        muted
-        loop
-        className="min-w-full min-h-full w-auto h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover z-0"
-      >
-        <source src={bg} type="video/mp4" />
-      </video>
-      <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.4)] z-10"></div>
+    <div
+      className="fixed top-0 left-0 w-full h-full bg-blend-multiply bg-no-repeat bg-center bg-cover"
+      style={{
+        backgroundImage: `url(${flagImage})`,
+      }}
+    >
     </div>
   );
 };
