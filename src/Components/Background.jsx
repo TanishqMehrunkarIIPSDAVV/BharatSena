a6a5a0111024cc95e2c9807e4c74ca8c5cd0a938
TanishqMehrunkarIIPSDAVV <mehrunkart@gmail.com>
2025-08-19 18:08:18 +0530

    title and front start

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -4,7 +4,7 @@ const Background = ()=>
 {
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full bg-blend-multiply bg-no-repeat bg-center bg-cover"
+      className="fixed top-0 left-0 w-full h-full bg-blend-multiply bg-no-repeat bg-center bg-cover bg-[rgba(0,0,0,0.3)]"
       style={{
         backgroundImage: `url(${flagImage})`,
       }}
