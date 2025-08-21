d563832335bde9832e0fc2c42cebef70fb8818c4
TanishqMehrunkarIIPSDAVV <mehrunkart@gmail.com>
2025-08-22 01:53:43 +0530

    some changes

--- src/Components/SideIcons.jsx
+++ src/Components/SideIcons.jsx
@@ -2,17 +2,21 @@ import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 
-const SideIcons=()=>
-{
-    return(
-        <div className="fixed top-[70px] left-0 z-[1] backdrop-blur-[10px] ml-[20px] mt-[25px]">
-            <div className="ml-[13px] w-[3px] h-[200px] bg-white"></div>
-            <FaInstagram className="font-black text-3xl mt-[20px] hover:cursor-pointer"/>
-            <FaFacebook className="font-black text-3xl mt-[20px] hover:cursor-pointer"/>
-            <FaYoutube className="font-black text-3xl mt-[20px] hover:cursor-pointer"/>
-            <div className="ml-[13px] w-[3px] h-[200px] bg-white mt-[20px]"></div>
+const SideIcons = () => {
+    return (
+        <div className="fixed top-[70px] left-0 z-[1] backdrop-blur-[10px] ml-[20px] mt-[25px] flex flex-col items-center">
+            {/* Top Divider */}
+            <div className="w-[3px] h-[200px] bg-white"></div>
+
+            {/* Icons */}
+            <FaInstagram className="font-black text-3xl mt-[20px] hover:cursor-pointer hover:text-orange-800 hover:scale-125 transition-transform duration-300" />
+            <FaFacebook className="font-black text-3xl mt-[20px] hover:cursor-pointer hover:text-blue-700 hover:scale-125 transition-transform duration-300" />
+            <FaYoutube className="font-black text-3xl mt-[20px] hover:cursor-pointer hover:text-green-800 hover:scale-125 transition-transform duration-300" />
+
+            {/* Bottom Divider */}
+            <div className="w-[3px] h-[200px] bg-white mt-[20px]"></div>
         </div>
     );
-}
+};
 
 export default SideIcons;
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <div className="absolute top-[150px] left-[150px] text-white z-[1]">
+    <div className="absolute top-[200px] left-[200px] text-white z-[1]">
       <div className="text-[100px] font-extrabold bg-gradient-to-b from-[#e65c00] via-[#f0f0f0] to-[#0b6623] text-transparent bg-clip-text drop-shadow-[0_3px_6px_rgba(0,0,0,0.6)]">
         Bharat
       </div>
